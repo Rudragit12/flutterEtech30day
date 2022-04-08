32c5f62d97816087249af19f30df9a13e912dad0
Rudragit12 <rudrachaudhary180010@gmail.com>
2022-04-08 17:17:12 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "30 days of flutter",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("day 1"),
+        ),
+        body: Center(
+          child: Text("I am taking this course"),
+        ),
+      ),
+    ),
+  );
+}
